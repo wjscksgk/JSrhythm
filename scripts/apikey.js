ee92ce7c0f1e5e82a2b3651c6d4e8f47949ae30f
MaTemPong <107092881+MaTemPong@users.noreply.github.com>
2023-09-26 15:35:12 +0900

    ,,,,

--- scripts/apikey.js
+++
@@ -1,4 +0,0 @@
-const config = {
-    youtube: "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBaIU3E7w4_cmohD3FsaXNH5Rh6ddyEEn4"
-}
-export default config;
--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,76 @@
-import config from './apikey'
+let keys = ["a", "s", "d", " ", "j", "k", "l"];
+const tileBtns = document.querySelectorAll(".tileBtn");
+const tileLines = document.querySelectorAll(".tileLine");
+const keyMap = {
+  "a": 0,
+  "s": 1,
+  "d": 2,
+  " ": 3,
+  "j": 4,
+  "k": 5,
+  "l": 6,
+};
 
-let keys = ['a','s','d',' ','j','k','l'];
-const tileBtns = document.querySelectorAll('.tileBtn')
-const tileLines = document.querySelectorAll('.tileLine')
-window.addEventListener('keydown', (e)=>{
-    keys.forEach((item, index) => {
-       if(item === e.key){
-        tileBtns[index].style.background = 'rgb(255,255,255) radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(254,255,0,1) 50%, rgba(250,151,2,1) 100%)'
-        tileBtns[index].style.boxShadow = '0px 0px 100px yellow'
-        tileBtns[index].style.transition = '0.1s'
-       } 
-    });
-})
-window.addEventListener('keyup', (e)=>{
-    keys.forEach((item, index) => {
-       if(item === e.key){
-        tileBtns[index].style.background = 'rgb(255,255,255)'
-        tileBtns[index].style.boxShadow = 'none'
-        tileBtns[index].style.transition = 'none'
-       } 
-    });
-})
+async function start(){
+  let bitmap;
+  fetch('./scripts/song.json').then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    bitmap = myJson;
+    console.log(bitmap)
+  });
+  await console.log(bitmap)
+}
 
-const youtube = document.getElementById('youtube');
-window.addEventListener('load', ()=>{
-    console.log('hello')
-    youtube.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
-})
-onYouTubeIframeAPIReady();
+start();
+
+
+const keyDownAnim = (idx) => {
+  tileBtns[idx].style.background ="rgb(255,255,255) radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(254,255,0,1) 50%, rgba(250,151,2,1) 100%)";
+  tileBtns[idx].style.boxShadow = "0px 0px 100px yellow";
+  tileBtns[idx].style.transition = "0.1s";
+
+  tileLines[idx].style.background = "rgba(247, 133, 0, 0.3)";
+  tileLines[idx].style.boxShadow = "0px 0px 100px rgba(0,0,0,0.5)";
+  tileLines[idx].style.transition = "0s";
+};
+
+const KeyUpAnim = (idx) => {
+  tileBtns[idx].style.background = "rgb(255,255,255)";
+  tileBtns[idx].style.boxShadow = "none";
+  tileBtns[idx].style.transition = "none";
+  
+  tileLines[idx].style.background = "none"
+  tileLines[idx].style.boxShadow = "none";
+  tileLines[idx].style.transition = "0.6s";
+};
+
+window.addEventListener("keydown", ({ key }) => {
+  const idx = keyMap[key];
+  !isNaN(idx) && keyDownAnim(idx);
+});
+window.addEventListener("keyup", ({ key }) => {
+  const idx = keyMap[key];
+  keys.includes(key) && KeyUpAnim(idx);
+});
+
+const tileAnim = (idx, type, time) => {
+  const tile = document.createElement('div')
+  tile.className = 'tile'
+  tileLines[idx].append(tile)
+  let yPos = 100
+  const startTime = performance.now()
+
+  const anim = (timestamp)=>{
+    const progress = (timestamp - startTime) / 1000
+    tile.style.bottom = `${yPos}%`
+    yPos = 100 - progress * 100
+    if(yPos > 0) requestAnimationFrame(anim)
+  }
+  requestAnimationFrame(anim)
+}
+
+tileAnim(1)
+tileAnim(3)
+tileAnim(5)
